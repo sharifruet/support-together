3a523931cc2ae111176cce17d534fb0719bfba52
Sharif Uddin <sharif@i2gether.com>
2024-05-05 16:43:28 +0600

    updated

--- node-api/index.js
+++ node-api/index.js
@@ -1,7 +1,11 @@
 // index.js
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 const db = require('./db'); // Import Sequelize instance
 
